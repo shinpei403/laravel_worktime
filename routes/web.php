58513a63c7e0bcaec33fcabf62ce8f9ac8955f84
shinpei403 <shiomi0726@outlook.jp>
2023-02-21 18:57:44 +0900

    管理者権限のミドルウェアの作成

--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,29 @@ use App\Http\Controllers\Worktime\WorktimeController;
 //ログイン後の画面
 Route::middleware(['auth'])->group(function () {
 
-  //従業員一覧画面を表示
-  Route::get('/user/index', [UserController::class, 'showUserIndex'])->name('userIndex');
-  
-  // 従業員登録画面を表示
-  Route::get('/user/create', [UserController::class, 'showUserCreate'])->name('userCreate');
-  
-  // 従業員登録
-  Route::post('/user/store', [UserController::class, 'exeUserStore'])->name('userStore');
-  
-  // 従業員編集画面を表示
-  Route::get('/user/edit/{id}', [UserController::class, 'showUserEdit'])->name('userEdit');
-  
-  // 従業員更新
-  Route::post('/user/update', [UserController::class, 'exeUserUpdate'])->name('userUpdate');
-  
-  // 従業員削除
-  Route::post('/user/delete/{id}', [UserController::class, 'exeUserDelete'])->name('userDelete');
-  
-  // 従業員詳細画面を表示
-  Route::get('/user/{id}', [UserController::class, 'showUserDetail'])->name('userDetail');
+  Route::middleware(['admin'])->group(function () {
+
+      //従業員一覧画面を表示
+      Route::get('/user/index', [UserController::class, 'showUserIndex'])->name('userIndex');
+      
+      // 従業員登録画面を表示
+      Route::get('/user/create', [UserController::class, 'showUserCreate'])->name('userCreate');
+      
+      // 従業員登録
+      Route::post('/user/store', [UserController::class, 'exeUserStore'])->name('userStore');
+      
+      // 従業員編集画面を表示
+      Route::get('/user/edit/{id}', [UserController::class, 'showUserEdit'])->name('userEdit');
+      
+      // 従業員更新
+      Route::post('/user/update', [UserController::class, 'exeUserUpdate'])->name('userUpdate');
+      
+      // 従業員削除
+      Route::post('/user/delete/{id}', [UserController::class, 'exeUserDelete'])->name('userDelete');
+      
+      // 従業員詳細画面を表示
+      Route::get('/user/{id}', [UserController::class, 'showUserDetail'])->name('userDetail');
+  });
 
   //勤怠登録画面を表示
   Route::get('worktime/create', [WorktimeController::class, 'showWorktimeCreate'])->name('worktimeCreate');
