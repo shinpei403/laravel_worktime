dd768661ba0760dd1237a6f793584fd5925ee73f
shinpei403 <shiomi0726@outlook.jp>
2023-02-22 21:16:43 +0900

    勤怠登録に時間を追加

--- resources/views/worktime/create.blade.php
+++ resources/views/worktime/create.blade.php
@@ -2,7 +2,7 @@
 @section('title', '勤怠登録')
 @section('content')
 <div class="row">
-    <div class="col-md-6 offset-3">
+    <div class="col-md-9 offset-1">
       <h2>勤怠登録</h2>
 
       @if(isset($worktime->working_flg) && $worktime->working_flg === 0)
@@ -13,34 +13,34 @@
 
       <x-alert type="success" :session="session('success')" />
       
+      <div class="text-center">
+        <div id="current-time" style="font-size: 70px;"></div>
+      </div>
+      </br>
       <form id="worktime-form" method="POST" action="{{ route('worktimeStore') }}">
           @csrf
-          <div class="form-group row">
-              <label for="start_time" class="col-md-4 col-form-label text-md-right">出勤時間</label>
-              <div class="col-md-6">
-                @if(isset($worktime->working_flg) && ($worktime->working_flg === 1 || $worktime->working_flg === 0))
-                    <button id="start-button" type="button" class="btn btn-primary" disabled>出勤する</button>
-                @else
-                    <button id="start-button" type="button" class="btn btn-primary">出勤する</button>
-                @endif
-              <input id="start_time" type="hidden" name="start_time">
-              </div>
-          </div>
-          <div class="form-group row">
-              <label for="end_time" class="col-md-4 col-form-label text-md-right">退勤時間</label>
-              <div class="col-md-6">
-                @if(isset($worktime->working_flg) && $worktime->working_flg === 1)
-                  <button id="end-button" type="button" class="btn btn-secondary">退勤する</button>
-                @else
-                  <button id="end-button" type="button" class="btn btn-secondary" disabled>退勤する</button>
-                @endif
-                  <input id="end_time" type="hidden" name="end_time">
-              </div>
-          </div>
-          <div class="form-group row mb-0">
-              <div class="col-md-6 offset-md-4">
-                  <button id="submit-button" type="submit" class="btn btn-primary" disabled>登録する</button>
-                  <input type='hidden' name='user_id' value="{{ Auth::user()->id }}">
+          <div class="form-group row justify-content-center">
+                <div class="col-md-6 d-flex">
+                    <div class="pr-3">
+                        @if(isset($worktime->working_flg) && ($worktime->working_flg === 1 || $worktime->working_flg === 0))
+                            <button id="start-button" type="button" class="btn btn-primary btn-lg" disabled>出勤する</button>
+                        @else
+                            <button id="start-button" type="button" class="btn btn-primary btn-lg">出勤する</button>
+                        @endif
+                        <input id="start_time" type="hidden" name="start_time">
+                    </div>
+                    <div class="pr-3">
+                        @if(isset($worktime->working_flg) && $worktime->working_flg === 1)
+                            <button id="end-button" type="button" class="btn btn-secondary btn-lg">退勤する</button>
+                        @else
+                            <button id="end-button" type="button" class="btn btn-secondary btn-lg" disabled>退勤する</button>
+                        @endif
+                        <input id="end_time" type="hidden" name="end_time">
+                    </div>
+                    <div>
+                        <button id="submit-button" type="submit" class="btn btn-primary btn-lg" disabled>登録する</button>
+                        <input type='hidden' name='user_id' value="{{ Auth::user()->id }}">
+                    </div>
               </div>
           </div>
       </form>
@@ -71,5 +71,26 @@
         endButton.disabled = true;
         submitButton.disabled = false;
     });
+
+    // 現在時刻を表示するための要素を取得する
+    const currentTimeElement = document.getElementById('current-time');
+
+    // 現在時刻を更新する関数を定義する
+    function updateCurrentTime() {
+        // 現在時刻を取得する
+        const now = new Date();
+        // 現在時刻を文字列に変換する
+        const options = {weekday: 'short', year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric'};
+        const currentTime = now.toLocaleString('ja-JP', options);
+        // 現在時刻を要素に表示する
+        currentTimeElement.textContent = currentTime;
+    }
+
+    // 現在時刻を初期表示する
+    updateCurrentTime();
+
+    // 1秒ごとに現在時刻を更新する
+    setInterval(updateCurrentTime, 1000);
+
 </script>
 @endsection
